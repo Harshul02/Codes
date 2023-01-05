eaf3970299ab544ce4f60adcd5d81c4fd76b4b35
Harshul Jain <harshulgangwal02@gmail.com>
2023-01-05 22:14:43 +0530

    Modified Bubble Sort

---
+++ Day 4/Modified BS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int N;
+    cout<<"Enter number of elements: ";
+    cin>>N;
+	int arr[N]; 
+    for(int i=0; i<N; i++)
+    {
+        cin>>arr[i];
+    }
+
+	for (int i = 0; i < N - 1; i++){
+        int swapped = 0;
+	    for (int j = 0; j < N - i - 1; j++){
+		    if (arr[j] > arr[j + 1]){
+			    swap(arr[j], arr[j + 1]);
+                swapped=1;
+        }
+        }
+        if(swapped == 0)
+        {
+            break;
+        }
+    }
+
+	cout << "Sorted array: \n";
+
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+	return 0;
+}
+
